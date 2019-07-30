9bdcab92e815b5bee1e31bd7b50014543ebb4ded
AldairMC <aldairmc2023@gmail.com>
2019-07-30 17:08:14 -0500

    Component We finished

---
+++ src/components/we/We.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './We.css'
+
+const We = () => {
+    return (
+        <div className="contenedor-nosotros">
+            <div className="imagen">
+                <img src="img/camisa_1.png" alt="imagen camisa"/>
+            </div>
+            <div className="contenido">
+                <h2>About We</h2>
+                <p>Loren fsdhfgh dhsgfh dgshf hdgfhgs hdfsghd fshdgf hsgdhfg hsdgfhgsdhfg hsdgfhsgdhfsgdjfjsdgfjhsgdhfgsdgf hsgdfhg sdhgfhsdgfhsgdhfsgd hfshdgfhsdgfj shgdfhsgdjhfsdhgfhsgdfhsdgfhsdgfhsgdfh gsdjfhgsdjhgfdhghsgfhsghgfhsgfjhgdhsgdjghj gfhsdgfhgd hsgf hsghjf gsdhgfsjdgfjsldfsdugd siufsdigf sidfsud</p>
+            </div>
+        </div>
+    );
+};
+
+export default We;
